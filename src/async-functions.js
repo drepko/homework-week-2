172b5f26145af48e69c1a753c56348854eb59c20
Danielle <repko.dm@gmail.com>
2019-02-23 21:26:35 +0100

    assignment 1 + 2 + 3

--- src/async-functions.js
+++ src/async-functions.js
@@ -2,12 +2,31 @@ function giveItBackLater(value, callback) {
         function loadComplete() {
             callback(value)
         }
-        setTimeout(loadComplete, 100)
+        setTimeout(loadComplete, 10)
     }
 
-function promiseToGiveItBackLater(){}
 
-function addSomePromises(){}
+function promiseToGiveItBackLater(value) {
+    return new Promise(function(resolve){
+        giveItBackLater(value, function(value) {
+            resolve(value)
+        })
+    })
+}
+
+function addSomePromises () {
+    const somePromise = new Promise(function(resolve, reject) {
+        resolve('')
+        reject('')
+    }) 
+  
+    somePromise.then(response => response.repeat(2))
+    .catch(error => error.repeat(3))
+  
+
+    return somePromise
+}
+
     
 module.exports.giveItBackLater = giveItBackLater
 module.exports.promiseToGiveItBackLater = promiseToGiveItBackLater
